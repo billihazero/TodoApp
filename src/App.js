6d011e52795d8fa34587b0e0d80208dfb275f9f0
billihazero <153509822+billihazero@users.noreply.github.com>
2024-11-04 23:02:30 +0900

    feat: Header component 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <div>todoeditor</div>
+      <div>todolist</div>
     </div>
   );
 }
