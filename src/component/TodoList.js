cd1bf14f7aa5302f43508193e0ead08a8accdfb6
billihazero <billihazero@gmail.com>
2024-11-11 14:59:27 +0900

    refactor: useMemo 변경

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
@@ -16,9 +16,29 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
           it.content.toLowerCase().includes(search.toLowerCase())
         );
   };
+
+  const analyzeTodo = useMemo(() => {
+    const totalCount = todo.length;
+    const doneCount = todo.filter((it) => it.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todo]);
+
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo;
+
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
+      <div>
+        <div>총 개수 : {totalCount}</div>
+        <div>완료한 일 : {doneCount}</div>
+        <div>아직 : {notDoneCount} </div>
+      </div>
       <input
         value={search}
         className="searchbar"
@@ -27,9 +47,10 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
       />
       <div className="list_wrapper">
         {getSearchResult().map((it) => (
-          <TodoItem 
-            key={it.id} {...it} 
-            onUpdate={onUpdate} 
+          <TodoItem
+            key={it.id}
+            {...it}
+            onUpdate={onUpdate}
             onDelete={onDelete}
           />
         ))}
